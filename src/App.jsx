ef470032b0ef8dce87ced0998795f486e289b05b
jonathan bushapa <jonathanbushapa@Mac-mini-7.home>
2024-02-23 13:05:33 -0600

    finish adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 // import { useState } from 'react'
-
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css'
+import Blue from "./components/Blue";
+import Red from "./components/Red";
+import Home from "./components/Home";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-   
+    <div id="container">
+        <h1></h1>
+        <div id="navbar">
+            <a href="/">Home</a>
+            <a href="/blue">blue</a>
+            <a href="/red">red</a>
+        </div>
+        <div id="main-section">
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/blue" element={<Blue />} />
+              <Route path="/red" element={<Red/>} />
+          </Routes>
+           
+        </div>
+    </div>
     </>
   )
 }
